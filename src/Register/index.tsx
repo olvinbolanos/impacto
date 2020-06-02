d5091041e11c75ca3e828a50d70eefdd9f8a2cad
Olvin <olvin.bolanos@yahoo.com>
2020-06-02 04:26:01 -0700

    beginning Impacto project with React

---
+++ src/Register/index.tsx
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import { withRouter, Redirect } from 'react-router-dom';
+
+class Register extends Component
+  
---
+++ src/constants/routes.jsx
@@ -0,0 +1,4 @@
+export const Landing = '/';
+export const Register = './Register';
+export const Home = './Home';
+export const Reviews = './Reviews';
